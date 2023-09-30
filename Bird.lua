5a41a80fe30649e996ff226ab66860b66e1471a6
gustavo1873 <guimaraesgustavo2003@gmail.com>
2023-09-30 17:33:06 -0300

    added bird and oop classes

---
+++ Bird.lua
@@ -0,0 +1,16 @@
+Bird = Class{}
+
+function Bird:init()
+    -- bird image and methods to get width and height
+    self.image = love.graphics.newImage('bird.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    -- position bird in the middle of the screen
+    self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+end
+
+function Bird:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end
